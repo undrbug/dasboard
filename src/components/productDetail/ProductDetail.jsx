d109944eed5fe6ef4d7973e1bb7ceee8dfce85a1
hGauna <undrbug@gmail.com>
2024-11-04 19:54:00 -0300

    Add card for producDetail

--- src/components/productDetail/ProductDetail.jsx
+++ src/components/productDetail/ProductDetail.jsx
@@ -1,21 +1,30 @@
 import { useParams } from "react-router-dom";
 import useFetch from '../../hooks/useFetch';
+import { URL_BASE_IMG } from "../../hooks/useFetch";
+import './ProductDetail.css';
 
 const ProductDetail = () => {
     const { id } = useParams();
 
-    const { data, loading, error } = useFetch(`/products/${id}`);
-
+    const { data: product, loading, error } = useFetch(`/products/${id}`);
     if (loading) return <p>Loading product...</p>;
     if (error) return;
 
     return (
-        <div>
-            <h2>Detalle del producto</h2>
-            <div className="product-detail">
-                <h3>{data?.product?.name}</h3>
-                <p>{data?.product?.description}</p>
-                <p>${data?.product?.price}</p>
+        <div className="product-card">
+            <div className="product-card-header">
+                <h3>{product.name}</h3>
+                <p className="product-brand">{product.brand}</p>
+            </div>
+            <div className="product-card-body">
+                <div className="image">
+                    <img src={`${URL_BASE_IMG}${product.Image}`} alt={product.name} />
+                </div>
+                <p className="product-description">{product.drink_description}</p>
+                <p>Tipo: <span>{product.drink_type}</span></p>
+                <p>Presentación: <span>{product.Presentation} ml</span></p>
+                <p>Precio: <span>${product.price}</span></p>
+                <p>Stock: <span>{product.Stock}</span></p>
             </div>
         </div>
     );
