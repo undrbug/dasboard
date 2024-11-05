188cb9a12e70b6235649ae50f0dd2466153867b8
Hgauna <hgauna@iprodha.misiones.gov.ar>
2024-11-05 13:39:43 -0300

    Two cards were added that later have to be components

--- src/pages/homeDashboard/HomeUsersDashboard.jsx
+++ src/pages/homeDashboard/HomeUsersDashboard.jsx
@@ -21,10 +21,22 @@ const HomeUserDashboard = () => {
             <div className="dashboard-container">
                 <div className="dashboard-panel">
                     <p>Total de Usuarios registrados: <span>{users.count}</span></p>
-                    <p>Total Páginas: <span>{users.totalPages}</span></p>
                     <p>Administradores: <span>{users.admins}</span></p>
                     <p>Activos: <span>{users.active}</span></p>
                     <p>Verificados: <span>{users.verified}</span></p>
+                </div>
+            </div>
+
+            <div className="dashboard-container">
+                <div className="dashboard-panel">
+                    <p>Total Páginas: <span>{users.totalPages}</span></p>
+                </div>
+            </div>
+
+            <div className="dashboard-container">
+                <div className="dashboard-panel">
+                    <p>Ultimo usuario registrado: <span>{users.lastAdded.email}</span></p>
+                    <p>Fecha: <span>{users.lastAdded.fecha}</span></p>
                 </div>
             </div>
         </>
